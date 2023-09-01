becb1fde15eb9d1b0c11f2a858eead2f61793c49
lukmanps <luqmansha6@gmail.com>
2023-09-01 10:22:12 +0530

    divisor Game

---
+++ 1025.js
@@ -0,0 +1,8 @@
+function divisorGame(n) {
+    if (n % 2=== 0) {
+        return true;
+    }
+    return false;
+}
+
+console.log(divisorGame(2));
--- 35.js
+++ 35.js
@@ -1,11 +1,13 @@
 function searchInsert(nums, target) {
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== target) {
-            while(nums[i]<target){
-                
-            }
-        } else {
-            return i;
+        if (nums[i] >= target) {
+            return i
         }
     }
+    return nums.length;
 }
+
+const nums = [1, 3, 5, 6];
+const target = 0;
+
+console.log(searchInsert(nums, target));
