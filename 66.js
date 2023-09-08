bf55d044cd2cb04e12fa29975ac50c4692af6978
lukmanps <luqmansha6@gmail.com>
2023-09-08 13:02:47 +0530

    plusOne

---
+++ 66.js
@@ -0,0 +1,20 @@
+function plusOne(digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i] = digits[i]+1;
+            return digits;
+        } else {
+            digits[i] = 0;
+        }
+    }
+    digits.unshift(1);
+    return digits;
+}
+
+
+
+const digits = [4,2,5,2];
+
+console.log(plusOne(digits));
+
+
